c155f899172d944488cb043383562d409745e88c
TooCoolCello <43973371+TooCoolCello@users.noreply.github.com>
2022-01-24 13:15:08 -0800

    Changed Name: Spontanious-Spores > ForagingPlus

--- src/main/java/com/toocoolcello/forageplus/ForagingPlus.java
+++ src/main/java/com/toocoolcello/forageplus/ForagingPlus.java
@@ -1,4 +1,4 @@
-package com.toocoolcello.sponspores;
+package com.toocoolcello.forageplus;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,14 +18,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(SpontaneousSpores.MOD_ID)
-public class SpontaneousSpores
+@Mod(ForagingPlus.MOD_ID)
+public class ForagingPlus
 {
-    public static final String MOD_ID = "sponspores";
+    public static final String MOD_ID = "forageplus";
     // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager
+            .getLogger();
 
-    public SpontaneousSpores() {
+    public ForagingPlus() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
